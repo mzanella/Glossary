eb3161bf6b0343677a585d4085224325eb299428
mzanella <zanna0150@gmail.com>
2016-03-07 18:19:17 +0100

    dkjavdff

---
+++ src/AutomaticGlossaryGenerator.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+perl glossario.pl;
+pdflatex -interaction=nonstopmode Glossario.tex;
+makeglossaries Glossario;
+pdflatex -interaction=nonstopmode Glossario.tex;
+pdflatex -interaction=nonstopmode Glossario.tex;
+rm Glossario.aux;
+rm Glossario.glo;
+rm Glossario.glg;
+rm Glossario.gls;
+rm Glossario.ist;
+rm Glossario.log;
+rm Glossario.out;
+cp Glossario.pdf ../Glossario.pdf;
+rm Glossario.pdf;
